7161284b78b3d6b5fb742180e19b5a77b5605a54
EC2 Default User <ec2-user@ip-172-31-24-80.ec2.internal>
2026-01-21 01:17:06 +0000

    login追加

---
+++ public/login2.php
@@ -0,0 +1,57 @@
+<?php
+ini_set('session.gc_maxlifetime', 86400);
+session_set_cookie_params(86400);
+session_start();
+
+$dbh = new PDO('mysql:host=mysql;dbname=example_db', 'root', '', [
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $email = $_POST['email'];
+    $input_password = $_POST['password'];
+
+    $select_sth = $dbh->prepare("SELECT * FROM users WHERE email = :email ORDER BY id DESC LIMIT 1");
+    $select_sth->execute([':email' => $email]);
+    $user = $select_sth->fetch(PDO::FETCH_ASSOC);
+
+if (empty($user) || empty($user['password'])) {
+    header("HTTP/1.1 303 See Other");
+    header("Location: ./login2.php?error=1");
+    exit;
+  }
+
+  $_SESSION['login_user_id'] = $user['id'];
+
+  header("HTTP/1.1 303 See Other");
+  header("Location: ./login_finish2.php");
+  exit;
+}
+?>
+
+初めての人は<a href="/signup2.php">会員登録</a>しましょう。
+<hr>
+
+<h1>ログイン</h1>
+
+<!-- ログインフォーム -->
+<form method="POST">
+  <label>
+    メールアドレス:
+    <input type="email" name="email">
+  </label>
+  <br>
+  <label>
+    パスワード:
+    <input type="password" name="password" minlength="6">
+  </label>
+  <br>
+  <button type="submit">決定</button>
+</form>
+
+<?php if(!empty($_GET['error'])): ?>
+  <div style="color: red;">
+    メールアドレスかパスワードが間違っています。
+  </div>
+<?php endif; ?>
+
